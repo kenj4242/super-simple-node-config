a0ebdc2e1bcf671603866b6774c9b24f322b266f
Ken Johnson <kenj4242@gmail.com>
2017-07-26 04:19:58 +0700

    Added default config paths. Added more tests.

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const fs = require('fs')
 
 module.exports = function(filename) {
-	filename = filename ? ifexists(filename) : ifexists(process.env.NODE_CONFIG);
+	if (filename) {
+		filename = ifexists(filename);
+	} else if (process.env.NODE_CONFIG) {
+		filename = ifexists(process.env.NODE_CONFIG);
+	} else {
+		filename = ifexists('config/'+(process.env.NODE_ENV || 'production')+'.json');
+	}
 	return require(filename);
 }
 
 function ifexists(filename) {
-	return fs.realpathSync(filename);
+	try {
+		return fs.realpathSync(filename);
+	} catch(e) {
+		if (e.code == 'ENOENT') {
+			e.mssg = "Config file not found: ("+filename+") "
+				+"Check your specified file path,"
+				+"or try setting the NODE_CONFIG environment variable to your config file path";
+			throw e;
+		}
+		throw e;
+	}
 }
 
 
--- test/index.js
+++ test/index.js
@@ -3,18 +3,43 @@ const conf = require("../index.js");
 
 describe('Basic', function() {
 
-	it('should return the config specified by env.NODE_CONFIG', function () {
-		var a = conf();
-		expect(a).to.have.property('testvalue');
-		expect(a.testvalue).to.equal('TEST VALUE');
-  });
-
 	it('should return the config specified at runtime', function () {
 		var a = conf('./test/conf2.json');
 		expect(a).to.have.property('testvalue');
 		expect(a.testvalue).to.equal('TEST VALUE 1');
   });
 
+	it('should return the config specified by env.NODE_CONFIG', function () {
+		var a = conf();
+		expect(a).to.have.property('testvalue');
+		expect(a.testvalue).to.equal('TEST VALUE');
+  });
+
+	it('should return the config derived from env.NODE_ENV', function () {
+		delete process.env.NODE_CONFIG;
+		var a = conf();
+		expect(a).to.have.property('testvalue');
+		expect(a.testvalue).to.equal('testing TEST VALUE');
+  });
+
+	it('should return the default production config', function () {
+		delete process.env.NODE_ENV;
+		var a = conf();
+		expect(a).to.have.property('testvalue');
+		expect(a.testvalue).to.equal('production TEST VALUE');
+  });
+
+	it('should fail if we give it a bad path', function () {
+		try {
+			var a = conf('./test/noconfigfile');
+		} catch(e) {
+			expect(e).to.be.an('error');
+			expect(e.code).to.equal('ENOENT');
+			return;
+		}
+
+		expect.fail('to throw an error', '');
+  });
 });
 
 
